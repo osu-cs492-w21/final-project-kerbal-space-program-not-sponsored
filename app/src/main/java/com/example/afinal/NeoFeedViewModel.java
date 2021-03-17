d2e0d898b061ae5f5d0f3d5fa1488887524627dc
Alex <alexander.rash99@gmail.com>
2021-03-16 23:12:22 -0700

    Error message added and just the spinner still needs to be added for loading indicator.

--- app/src/main/java/com/example/afinal/NeoFeedViewModel.java
+++ app/src/main/java/com/example/afinal/NeoFeedViewModel.java
@@ -4,24 +4,27 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.afinal.data.AsteroidRepository;
+import com.example.afinal.data.LoadingStatus;
 import com.example.afinal.data.NeoFeed;
 
 public class NeoFeedViewModel extends ViewModel {
     private AsteroidRepository repository;
     private LiveData<NeoFeed> neoFeed;
-    //loading status
+    private LiveData<LoadingStatus> loadingStatus;
 
     public NeoFeedViewModel() {
         this.repository = new AsteroidRepository();
         this.neoFeed = repository.getNeoFeed();
-        //loading status
+        this.loadingStatus = repository.getLoadingStatus();
     }
 
     public LiveData<NeoFeed> getNeoFeed() {
         return this.neoFeed;
     }
 
-    //return loading status
+    public LiveData<LoadingStatus> getLoadingStatus() {
+        return this.loadingStatus;
+    }
 
     public void loadNeoFeed(String date, String apiKey) {
         this.repository.loadNeoFeed(date, date, apiKey);
--- app/src/main/java/com/example/afinal/data/AsteroidRepository.java
+++ app/src/main/java/com/example/afinal/data/AsteroidRepository.java
@@ -1,5 +1,6 @@
 package com.example.afinal.data;
 
+import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.lifecycle.LiveData;
@@ -19,15 +20,19 @@ public class AsteroidRepository {
     private static final String BASE_URL = "https://www.neowsapp.com/";
 
     private MutableLiveData<NeoFeed> neoFeed;
-//    private MutableLiveData<LoadingStatus> loadingStatus;
+    private MutableLiveData<LoadingStatus> loadingStatus;
 
-    private String currentDate;
+    private String currentStartDate;
+    private String currentEndDate;
 
     private NeoFeedService neoFeedService;
 
     public AsteroidRepository(){
         this.neoFeed = new MutableLiveData<>();
         this.neoFeed.setValue(null);
+
+        this.loadingStatus = new MutableLiveData<>();
+        this.loadingStatus.setValue(LoadingStatus.SUCCESS);
 
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(AsteroidData.class, new AsteroidData.JsonDeserializer())
@@ -43,34 +48,74 @@ public class AsteroidRepository {
         return this.neoFeed;
     }
 
+    public LiveData<LoadingStatus> getLoadingStatus(){
+        return this.loadingStatus;
+    }
+
     public void loadNeoFeed(String startDate, String endDate, String apiKey){
-        this.neoFeed.setValue(null);
-        Log.d(TAG, "getting new feed for this query: " + startDate + apiKey);
-
-        Call<NeoFeed> req = this.neoFeedService.fetchNeoFeed(startDate, endDate, apiKey);
-        Log.d(TAG, "getting new feed for this call: " + req);
-        req.enqueue(new Callback<NeoFeed>() {
-            @Override
-            public void onResponse(Call<NeoFeed> call, Response<NeoFeed> response) {
-                if(response.code() == 200) {
-                    neoFeed.setValue(response.body());
-                    //loading status value set success
+        if(shouldFetchNeoFeed(startDate, endDate)) {
+            this.currentStartDate = startDate;
+            this.currentEndDate = endDate;
+            this.neoFeed.setValue(null);
+            this.loadingStatus.setValue(LoadingStatus.LOADING);
+            Log.d(TAG, "getting new feed for this query: " + startDate);
+            Call<NeoFeed> req = this.neoFeedService.fetchNeoFeed(startDate, endDate, apiKey);
+//            Log.d(TAG, "getting new feed for this call: " + req);
+            req.enqueue(new Callback<NeoFeed>() {
+                @Override
+                public void onResponse(Call<NeoFeed> call, Response<NeoFeed> response) {
+                    if(response.code() == 200) {
+                        neoFeed.setValue(response.body());
+                        loadingStatus.setValue(LoadingStatus.SUCCESS);
+                    }
+                    else {
+                        loadingStatus.setValue(LoadingStatus.ERROR);
+                        Log.d(TAG, "unsuccessful API request(code!=200): " + call.request().url());
+                        Log.d(TAG, "----response status code: " + response.code());
+                        Log.d(TAG, "----response: " + response.toString());
+                    }
                 }
-                else {
-                    //loading status value set error
-                    Log.d(TAG, "unsuccessful API request(code!=200): " + call.request().url());
-                    Log.d(TAG, "  -- response status code: " + response.code());
-                    Log.d(TAG, "  -- response: " + response.toString());
-                }
-            }
 
-            @Override
-            public void onFailure(Call<NeoFeed> call, Throwable t) {
-                //loading status value set error
-                Log.d(TAG, "unsuccessful API request(failure): " + call.request().url());
-                t.printStackTrace();
-            }
-        });
+                @Override
+                public void onFailure(Call<NeoFeed> call, Throwable t) {
+                    Log.d(TAG, "unsuccessful API request(failure): " + call.request().url());
+                    t.printStackTrace();
+                    loadingStatus.setValue(LoadingStatus.ERROR);
+                }
+            });
+        }
+        else {
+            Log.d(TAG, "using cached results for these parameters. startDate: " + startDate + "  endDate: " + endDate);
+        }
+
+
+
+    }
+
+    private boolean shouldFetchNeoFeed(String startDate, String endDate) {
+        /*
+         * Fetch NeoFeed if there isn't currently one stored.
+         */
+        NeoFeed currentNeoFeed = this.neoFeed.getValue();
+        if (currentNeoFeed == null) {
+            return true;
+        }
+
+        /*
+         * Fetch NeoFeed if there was an error fetching the last one.
+         */
+        if (this.loadingStatus.getValue() == LoadingStatus.ERROR) {
+            return true;
+        }
+
+        /*
+         * Fetch NeoFeed if either of the dates have changed
+         */
+        if (!TextUtils.equals(startDate, this.currentStartDate) || !TextUtils.equals(endDate, this.currentEndDate)) {
+            return true;
+        }
+
+        return false;
     }
 
 }
