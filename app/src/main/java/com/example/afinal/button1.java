0792098ff29bd936022a26b21319db54d681c627
blakele <49256995+blakele@users.noreply.github.com>
2021-03-17 04:43:11 -0700

    changed button names to match apis

--- app/src/main/java/com/example/afinal/button1.java
+++ app/src/main/java/com/example/afinal/button1.java
@@ -97,7 +97,6 @@ public class button1 extends AppCompatActivity implements View.OnClickListener, 
                     Log.i("Video", "Video Playing....");
                 }
             });
-
         }
 
         title = findViewById(R.id.picture_title);
